6621f2e8b2a4495f75f439c168f91ca54bc860da
RyougiGuda <ryougiguda@gmail.com>
2024-03-12 22:50:46 +0800

    3.12

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,20 +1,22 @@
-model=<multilingual model>
-source_lang=<source language>
-target_lang=<target language>
+model=../model/checkpoint_best.pt
 lang_pairs="ar-de,de-ru,en-fr,en-ru,en-zh,es-fr"
 lang_list=../data/lang_list.txt
-
-
+path_2_data=../data/processed_path
+source_lang=en
+target_lang=zh
 
 fairseq-generate $path_2_data \
   --path $model \
   --task translation_multi_simple_epoch \
   --gen-subset test \
   --source-lang $source_lang \
-  --target-lang $target_lang
+  --target-lang $target_lang  \
   --sacrebleu --remove-bpe 'sentencepiece'\
   --batch-size 32 \
   --encoder-langtok "src" \
   --decoder-langtok \
   --lang-dict "$lang_list" \
-  --lang-pairs "$lang_pairs" > ${source_lang}_${target_lang}.txt
+  --lang-pairs "$lang_pairs" > ../data/test_gen/${source_lang}_${target_lang}.txt
+  
+
+
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,22 +1,19 @@
 # Train a multilingual transformer model
 path_2_data="../data/processed_path"
-lang_pairs="ar-de,de-ru,en-fr,en-ru,en-zh,es-fr"
-lang_list=../data/lang_list.txt
 
-fairseq-train "$path_2_data" \
-  --encoder-normalize-before --decoder-normalize-before \
-  --arch transformer --layernorm-embedding \
-  --task translation_multi_simple_epoch \
-  --sampling-method "temperature" \
-  --sampling-temperature 1.5 \
-  --encoder-langtok "src" \
-  --decoder-langtok \
-  --lang-dict "$lang_list" \
-  --lang-pairs "$lang_pairs" \
-  --criterion label_smoothed_cross_entropy --label-smoothing 0.2 \
-  --optimizer adam --adam-eps 1e-06 --adam-betas '(0.9, 0.98)' \
-  --lr-scheduler inverse_sqrt --lr 3e-05 --warmup-updates 2500  \
-  --dropout 0.3 --attention-dropout 0.1 --weight-decay 0.0 \
-  --max-tokens 2048 --update-freq 2 \
-  --save-interval 1 --save-interval-updates 5000 --keep-interval-updates 10 --no-epoch-checkpoints \
-  --seed 222 --log-format simple --log-interval 2 --save-dir ../model
+
+  
+CUDA_VISIBLE_DEVICES=0 fairseq-train \
+    ${path_2_data} \
+    --arch transformer --share-decoder-input-output-embed \
+    --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
+    --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
+    --dropout 0.3 --weight-decay 0.0001 \
+    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
+    --max-tokens 4096 \
+    --eval-bleu \
+    --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
+    --eval-bleu-detok moses \
+    --eval-bleu-remove-bpe \
+    --eval-bleu-print-samples \
+    --best-checkpoint-metric bleu --maximize-best-checkpoint-metric
