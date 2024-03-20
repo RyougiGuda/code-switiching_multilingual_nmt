33c6ab348238bcf019b244ff496ba4b5cc7eaf45
RyougiGuda <ryougiguda@gmail.com>
2024-03-20 23:28:52 +0800

    3.20

--- scripts/train_code_switching/bleu.sh
+++ scripts/train_code_switching/bleu.sh
@@ -1,6 +1,6 @@
 MULTI_BLEU=../../requirements/mosesdecoder/scripts/generic/multi-bleu.perl
 
-    
+out_path=../../evaluate/code_switching
 
-${MULTI_BLEU} -lc true.txt < predict.txt
+${MULTI_BLEU} -lc ${out_path}/true.txt < ${out_path}/predict.txt
 
